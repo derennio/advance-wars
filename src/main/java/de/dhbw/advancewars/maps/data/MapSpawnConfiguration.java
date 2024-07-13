efb839f25460b5c19254b19632f66ee3604fd965
Ennio Haibach <ennio94@gmx.de>
2024-07-13 17:12:46 +0200

    Convert map spawn config to record class

--- src/main/java/de/dhbw/advancewars/maps/data/MapSpawnConfiguration.java
+++ src/main/java/de/dhbw/advancewars/maps/data/MapSpawnConfiguration.java
@@ -1,30 +1,10 @@
 package de.dhbw.advancewars.maps.data;
 
+import java.io.Serial;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-// TODO: will later on become a record --> needs to be editable for inital spawn configurations.
-public class MapSpawnConfiguration implements Serializable {
+public record MapSpawnConfiguration(String mapName, List<SpawnDTO> spawns) implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
-
-    private String mapName;
-    private List<SpawnDTO> spawns = new ArrayList<>();
-
-    public List<SpawnDTO> getSpawns() {
-        return spawns;
-    }
-
-    public void addSpawn(SpawnDTO spawn) {
-        spawns.add(spawn);
-    }
-
-    public void setMapName(String mapName) {
-        this.mapName = mapName;
-    }
-
-    public String getMapName() {
-        return mapName;
-    }
-
 }
